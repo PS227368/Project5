c1f8089d155141b5ed80defe2378671f7b3c34f4
PS229589 <PS229589@summacollege.nl>
2023-06-23 14:04:45 +0200

    Bottom nav toegevoegd

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/welcome_view.dart';
+//import 'package:my_app/welcome_view.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MyApp1());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp1 extends StatelessWidget {
+  const MyApp1({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeView(),
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home page'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Oefeningen',
+              icon: Icon(Icons.fitness_center),
+            ),
+            BottomNavigationBarItem(
+              label: 'Inloggen',
+              icon: Icon(Icons.login),
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
--- lib/pages/welcome_view.dart
+++ lib/pages/welcome_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+//import 'package:google_nav_bar/google_nav_bar.dart';
 
 //import '../Login/inlog.dart';
 
 class WelcomeView extends StatefulWidget {
-  const WelcomeView({Key? key, required AppBar appBar}) : super(key: key);
+  const WelcomeView({Key? key}) : super(key: key);
 
   @override
   State<WelcomeView> createState() => _WelcomeViewState();
@@ -15,22 +15,6 @@ class _WelcomeViewState extends State<WelcomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: const GNav(
-        tabs: [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
-          ),
-          GButton(
-            icon: Icons.fitness_center_rounded,
-            text: 'Fitness',
-          ),
-          GButton(
-            icon: Icons.settings,
-            text: 'Settings',
-          ),
-        ],
-      ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -70,32 +54,32 @@ class _WelcomeViewState extends State<WelcomeView> {
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     // Navigeer naar de andere pagina wanneer de knop wordt ingedrukt
-      //     Navigator.push(
-      //       context,
-      //       MaterialPageRoute(builder: (context) => const Inlog()),
-      //     );
-      //   },
-      //   child: const Icon(Icons.arrow_forward),
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigeer naar de andere pagina wanneer de knop wordt ingedrukt
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const OtherPage()),
+          );
+        },
+        child: const Icon(Icons.arrow_forward),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
 
-// class OtherPage extends StatelessWidget {
-//   const OtherPage({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Inlog Pagina'),
-//       ),
-//       body: const Center(
-//         child: Text(''),
-//       ),
-//     );
-//   }
-// }
+class OtherPage extends StatelessWidget {
+  const OtherPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inlog Pagina'),
+      ),
+      body: const Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
